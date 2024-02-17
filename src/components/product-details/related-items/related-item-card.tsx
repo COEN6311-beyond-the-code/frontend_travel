1c325c5ddf0e0626c4323c2e81631e40e7874147
alfredasare <alfredasare101@gmail.com>
2024-02-16 23:23:59 -0500

    Fix nested link issue

--- src/components/product-details/related-items/related-item-card.tsx
+++ src/components/product-details/related-items/related-item-card.tsx
@@ -3,43 +3,45 @@ import Link from 'next/link';
 
 const RelatedItemCard = ({ product }: { product: Product }) => {
 	return (
-		<Link href={`/item/${product.id}`}>
-			<div className='relative'>
-				<div className='relative h-72 w-full overflow-hidden rounded-lg'>
-					<img
-						src={product.imageSrc}
-						alt={product.imageAlt}
-						className='h-full w-full object-cover object-center'
-					/>
+		<div>
+			<Link href={`/item/${product.id}`}>
+				<div className='relative'>
+					<div className='relative h-72 w-full overflow-hidden rounded-lg'>
+						<img
+							src={product.imageSrc}
+							alt={product.imageAlt}
+							className='h-full w-full object-cover object-center'
+						/>
+					</div>
+					<div className='relative mt-4'>
+						<h3 className='text-sm font-medium text-gray-900'>
+							{product.name}
+						</h3>
+						<p className='mt-1 text-sm text-gray-500'>
+							{product.options}
+						</p>
+					</div>
+					<div className='absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4'>
+						<div
+							aria-hidden='true'
+							className='absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50'
+						/>
+						<p className='relative text-lg font-semibold text-white'>
+							$ {product.price}
+						</p>
+					</div>
 				</div>
-				<div className='relative mt-4'>
-					<h3 className='text-sm font-medium text-gray-900'>
-						{product.name}
-					</h3>
-					<p className='mt-1 text-sm text-gray-500'>
-						{product.options}
-					</p>
-				</div>
-				<div className='absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4'>
-					<div
-						aria-hidden='true'
-						className='absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50'
-					/>
-					<p className='relative text-lg font-semibold text-white'>
-						$ {product.price}
-					</p>
-				</div>
-			</div>
+			</Link>
 			<div className='mt-6'>
-				<a
-					href={product.href}
+				<Link
+					href={`/checkout/${product.id}`}
 					className='relative flex items-center justify-center rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200'
 				>
 					Purchase package
 					<span className='sr-only'>, {product.name}</span>
-				</a>
+				</Link>
 			</div>
-		</Link>
+		</div>
 	);
 };
 
